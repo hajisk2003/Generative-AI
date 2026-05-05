ce6aa79c6611ebed06bc935eae2ae57190c3324d
Haji Shaikh <hajisk89999@gmail.com>
2026-05-05 11:22:47 +0530

    Integrated PromptTemplate from LangChain to manage structured LLM inputs.

--- LANGCHAIN PROMPTS/prompt_ui.py
+++ LANGCHAIN PROMPTS/prompt_ui.py
@@ -1,13 +1,28 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-import streamlit as st 
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
+model = ChatOpenAI()
 
-st.header("Reasearch Tool")
+st.header('Reasearch Tool')
 
-user_input=st.text_input("enter your Prompt")
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
-if st.button("summarize"):
-    res=model.invoke(user_input)
-    st.write(res.content)
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = load_prompt('template.json')
+
+
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.content)
