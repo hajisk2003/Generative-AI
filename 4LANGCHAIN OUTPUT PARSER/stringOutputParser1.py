694c0e658a04f1186921deb8dcb4bc1f59a52508
hajisk2003 <hajisk89999@gmail.com>
2026-05-12 11:53:12 +0530

    implementing String Output parser

---
+++ 4LANGCHAIN OUTPUT PARSER/stringOutputParser1.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+template1=PromptTemplate(
+    template='write a detail report on {topic}',
+    input_variables=['topic']
+)
+template2=PromptTemplate(
+    template='write a 5 line summary on text. /n report on {text}',
+    input_variables=['text']
+)
+
+parser=StrOutputParser()
+chain=template1 | model | parser |  template2 | model |  parser
+res=chain.invoke({'topic':'black hole'})
+print(res)
