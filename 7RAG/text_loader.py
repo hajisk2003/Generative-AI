1c698a2ee9cb88ee7499e4e6d46f5bae7e80ed08
hajisk2003 <hajisk89999@gmail.com>
2026-05-17 11:22:06 +0530

    Text Loader in RAG

---
+++ 7RAG/text_loader.py
@@ -0,0 +1,31 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+model=ChatOpenAI()
+
+prompt=PromptTemplate(
+    template='write a summary on the following poem \n {poem}',
+    input_variables=['poem']
+)
+
+parser=StrOutputParser()
+
+chain=prompt | model | parser
+
+
+
+loader = TextLoader('cricket.txt', encoding='utf-8')
+
+
+docs = loader.load()
+
+print(chain.invoke({'poem':docs[0].page_content}))
+
+print(docs)
+
+print(docs[0].page_content)
+print(docs[0].metadata)
